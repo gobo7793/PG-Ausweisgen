7643f71f6ff69bd25daadfcebb6b492747733f5e
Gerald Siegert <gerald.siegert@outlook.com>
2020-12-23 22:14:58 +0100

    added icon

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using Microsoft.WindowsAPICodePack.Dialogs;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
